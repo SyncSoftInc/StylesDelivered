71522032b38d44a910ab5d09ca2c31a1635fb0d9
Lukiya Chen <lukiya@live.com>
2019-09-09 09:30:26 -0700

    update

---
+++ src/Shared/Core/DTO/Product/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using SyncSoft.StylesDelivered.DTO.Customer;
+using System;
+using System.Collections.Generic;
+
+namespace SyncSoft.StylesDelivered.DTO.Product
+{
+    public class ProductDTO
+    {
+        public string SKU { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int InvQty { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public IList<CustomerDTO> Customers { get; set; }
+    }
+}
