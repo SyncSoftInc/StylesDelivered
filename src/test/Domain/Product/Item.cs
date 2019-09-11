a6e29855641fa27ced5c7c49115d64de6a704640
Jonathan Poon <jonathan.poon@syncsoftinc.com>
2019-09-10 17:47:42 -0700

    update

--- src/test/Domain/Product/Item.cs
+++ src/test/Domain/Product/Item.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using SyncSoft.App.Components;
+using SyncSoft.StylesDelivered.DataAccess.Product;
 using SyncSoft.StylesDelivered.Domain.Product;
 using System;
 using System.Threading.Tasks;
@@ -11,12 +12,33 @@ namespace Product
         private static readonly Lazy<IProductItemService> _lazyProductItemService = ObjectContainer.LazyResolve<IProductItemService>();
         private IProductItemService ProductItemService => _lazyProductItemService.Value;
 
+        private static readonly Lazy<IProductItemDAL> _lazyProductItemDAL = ObjectContainer.LazyResolve<IProductItemDAL>();
+        private IProductItemDAL ProductItemDAL => _lazyProductItemDAL.Value;
+
         [Test]
         public async Task SyncInventories()
         {
             var msgCode = await ProductItemService.SyncInventoriesAsync().ConfigureAwait(false);
 
             Assert.IsTrue(msgCode.IsSuccess());
+        }
+
+        [Test]
+        public async Task InsertItems()
+        {
+            var asin = "new_2";
+
+            for (int i = 0; i < 5; i++)
+            {
+                var msgCode = await ProductItemDAL.InsertItemAsync(new SyncSoft.StylesDelivered.DTO.Product.ProductItemDTO
+                {
+                    ASIN = asin,
+                    SKU = $"new_item2_{i}",
+                    Color = $"color_{i}",
+                    Size = $"size_{i}"
+                }).ConfigureAwait(false);
+                Assert.IsTrue(msgCode.IsSuccess());
+            }
         }
     }
 }
