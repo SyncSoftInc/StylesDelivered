3200c83e49a86aedd5786f096c2284b37787af03
Lukiya Chen <lukiya@live.com>
2019-09-18 13:35:14 -0700

    update

--- src/Business/Domain/Order/ShipOrder/ShipOrderTransaction.cs
+++ src/Business/Domain/Order/ShipOrder/ShipOrderTransaction.cs
@@ -19,7 +19,7 @@ namespace SyncSoft.StylesDelivered.Domain.Order.ShipOrder
             , new ShipConfirmActivity()
         )
         {
-            Context.Set(CONSTANTS.TRANSACTIONS.EntryCommand, cmd);
+            SetState(CONSTANTS.TRANSACTIONS.EntryCommand, cmd);
         }
     }
 }
